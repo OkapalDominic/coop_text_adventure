0007c5f52e5f85f04612400ccf7c481c78d7b59a
William Olsen <wolsen@pdx.edu>
2019-05-31 17:02:32 -0700

    added basic item and tests to go with it

--- src/components/scratch/TestDriver.ts
+++ src/components/scratch/TestDriver.ts
@@ -4,7 +4,9 @@
 
 import Dungeon from './dungeon'
 import Area from './area'
+import Item from './item'
 
+// figureng out if basic features of Area are working
 console.log('--------Testing Area--------');
 let a = new Area();
 console.log(a.getTitle());
@@ -17,6 +19,19 @@ a.enter();
 a.leave();
 console.log('--------Done Area--------');
 
+// figureng out if basic features of Item are working
+console.log('--------Testing Item--------');
+let i = new Item();
+console.log(i.getTitle());
+i.setTitle('WonderfulItem');
+console.log(i.getTitle());
+console.log(i.getDescription());
+i.setDescription('It probably does things!');
+console.log(i.getDescription());
+console.log('--------Done Item--------');
+
+/*
+// figureng out if basic features of Dungeon are working
 console.log('--------Testing Dungeon--------');
 let d = new Dungeon();
 console.log(d.getTitle());
@@ -35,5 +50,6 @@ d.removeArea('InterestingArea');
 console.log(d.getElementAreas(0));
 console.log(d.getCurrentArea());
 console.log('--------Done Dungeon--------');
+*/
 
 console.log('End of tests.');
---
+++ src/components/scratch/item.ts
@@ -0,0 +1,33 @@
+//items that can be picked up/used
+
+class Item {
+	// name/description dungeon
+	private title: string; 
+	private description: string;
+	
+	constructor() {
+		this.title = 'BoringItem';
+		this.description = 'Seems to be broken';
+	}
+	
+	// getter/setter for Area title
+	// note area name should not contain spaces
+	getTitle(): string {
+		if (/\s/.test(this.title)) {
+			console.log(`Warning whiteSpace detected in "${this.title}"`);
+		}
+		return this.title;
+	}
+	setTitle(newTitle: string): void {
+		if (/\s/.test(newTitle)) {
+			console.log(`Warning whiteSpace detected in "${newTitle}"`);
+		}
+		this.title = newTitle;
+	}
+	
+	// getter/setter for Area description
+	getDescription(): string { return this.description; }
+	setDescription(newDesc: string): void { this.description = newDesc; }
+}
+
+export default Item;
