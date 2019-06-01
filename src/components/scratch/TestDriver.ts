6a156c4d662b211844cabff02768549b1934c8f9
William Olsen <wolsen@pdx.edu>
2019-06-01 02:20:53 -0700

    Revamped entity... pretty sure I did bad things... but it sure seems neat

--- src/components/scratch/TestDriver.ts
+++ src/components/scratch/TestDriver.ts
@@ -2,90 +2,151 @@
 // cd compile
 // node TestDriver.js
 
+import Entity from './entity'
 import Dungeon from './dungeon'
 import Area from './area'
 import Item from './item'
 
-// figureng out if basic features of Item are working
-console.log('--------Testing Item--------');
-// create item
-let i = new Item();
-// test title setting
-console.log(i.getTitle());
-i.setTitle('WonderfulItem');
-console.log(i.getTitle());
-// test description setting
-console.log(i.getDescription());
-i.setDescription('It probably does things!');
-console.log(i.getDescription());
-console.log('--------Done Item--------');
-
-// figureng out if basic features of Area are working
-console.log('--------Testing Area--------');
-// create area
-let a = new Area();
-// test title setting
-console.log(a.getTitle());
-a.setTitle('InterestingArea');
-console.log(a.getTitle());
-// test description setting
-console.log(a.getDescription());
-a.setDescription('A very interesting place');
-console.log(a.getDescription());
-// test enter leave
-a.enter();
-a.leave();
-// test item
-a.removeItem('WonderfulItem');
-a.addItem(i);
-console.log(a.getItem(0));
-a.removeItem('WonderfulItem');
-console.log(a.getItem(0));
-a.addItem(i);
-a.addItem(i);
-console.log(a.getItem(0));
-console.log(a.getItem(1));
-console.log('--------Done Area--------');
-
-// figureng out if basic features of Dungeon are working
-console.log('--------Testing Dungeon--------');
-// create dungeon
-let d = new Dungeon();
-// test title setting
-console.log(d.getTitle());
-d.setTitle('WonderfulDungeon');
-console.log(d.getTitle());
-// test description setting
-console.log(d.getDescription());
-d.setDescription('A sense of wonder fills the air!');
-console.log(d.getDescription());
-// tets bad cmd parse
-d.parseCommand('Baloney Beef Veal Pork');
-// test area adding
-d.parseCommand('enter InterestingArea');
-d.addArea(a);
-d.parseCommand('enter InterestingArea');
-// test area double add
-d.addArea(a);
-console.log(d.getCurrentArea());
-// test area removal
-d.removeArea('InterestingArea');
-console.log(d.getElementAreas(0));
-// test get current area
-console.log(d.getCurrentArea());
-// test item adding/removing
-d.removeItem('WonderfulItem');
-d.addItem(i);
-console.log(d.getItem(0));
-d.removeItem('WonderfulItem');
-console.log(d.getItem(0));
-d.addItem(i);
-d.addItem(i);
-console.log(d.getItem(0));
-console.log(d.getItem(1));
-d.removeItem('WonderfulItem');
-console.log(d.getItem(0));
-console.log(d.getItem(1));
-console.log('--------Done Dungeon--------');
-
+// ----------------------------------
+// tests to run
+// ----------------------------------
+console.log('Start of tests.');
+testEntity();
 console.log('End of tests.');
+
+// ----------------------------------
+// testing out basic entity class
+// ----------------------------------
+function testEntity(): void {
+	console.log('--------Testing Entity--------');
+	
+	let e = new Entity('test', 'testing entity', undefined);
+	// display entity
+	console.log(e);
+	
+	// test adding a state
+	console.log('getState when no "meat": ', e.getState('meat'));
+	console.log('addState "meat": ', e.addState('meat', ['Baloney', 'Beef', 'Veal', 'Pork']));
+	console.log('getState is "meat": ', e.getState('meat'));
+	console.log('addState "meat" aleady exists: ', e.addState('meat', ['Baloney', 'Beef', 'Veal', 'Pork']));
+	
+	// test removing a state
+	console.log('removeState "meat": ', e.removeState('meat'));
+	console.log('getState when no "meat": ', e.getState('meat'));
+	console.log('removeState nonexistent "veggies": ', e.removeState('veggies'));
+	
+	// test behavior
+	console.log('onEnter undefined: ', e.onEnter());
+	console.log('setOnEnter: ', e.setOnEnter(function() {
+		console.log('NOTICE ME');
+		console.log('NOTICE ME');
+		console.log(this.name);
+		console.log('NOTICE ME');
+		console.log('NOTICE ME');
+	}));
+	console.log('onEnter defined: ', e.onEnter());
+	
+	// display entity
+	console.log(e);
+	
+	console.log('--------Done Entity--------');
+}
+
+/*
+// ----------------------------------
+// figureng out if basic features of Item are working
+// outdated
+// ----------------------------------
+function testItem(): void {
+	console.log('--------Testing Item--------');
+	// create item
+	let i = new Item();
+	// test title setting
+	console.log(i.getTitle());
+	i.setTitle('WonderfulItem');
+	console.log(i.getTitle());
+	// test description setting
+	console.log(i.getDescription());
+	i.setDescription('It probably does things!');
+	console.log(i.getDescription());
+	console.log('--------Done Item--------');
+}
+
+// ----------------------------------
+// figureng out if basic features of Area are working
+// outdated
+// ----------------------------------
+function testArea(): void {
+	console.log('--------Testing Area--------');
+	// create area
+	let a = new Area();
+	// test title setting
+	console.log(a.getTitle());
+	a.setTitle('InterestingArea');
+	console.log(a.getTitle());
+	// test description setting
+	console.log(a.getDescription());
+	a.setDescription('A very interesting place');
+	console.log(a.getDescription());
+	// test enter leave
+	a.enter();
+	a.leave();
+	// test item
+	a.removeItem('WonderfulItem');
+	a.addItem(i);
+	console.log(a.getItem(0));
+	a.removeItem('WonderfulItem');
+	console.log(a.getItem(0));
+	a.addItem(i);
+	a.addItem(i);
+	console.log(a.getItem(0));
+	console.log(a.getItem(1));
+	console.log('--------Done Area--------');
+}
+
+// ----------------------------------
+// figureng out if basic features of Dungeon are working
+// outdated
+// ----------------------------------
+function testDungeon(): void {
+	console.log('--------Testing Dungeon--------');
+	// create dungeon
+	let d = new Dungeon();
+	// test title setting
+	console.log(d.getTitle());
+	d.setTitle('WonderfulDungeon');
+	console.log(d.getTitle());
+	// test description setting
+	console.log(d.getDescription());
+	d.setDescription('A sense of wonder fills the air!');
+	console.log(d.getDescription());
+	// tets bad cmd parse
+	d.parseCommand('Baloney Beef Veal Pork');
+	// test area adding
+	d.parseCommand('enter InterestingArea');
+	d.addArea(a);
+	d.parseCommand('enter InterestingArea');
+	// test area double add
+	d.addArea(a);
+	console.log(d.getCurrentArea());
+	// test area removal
+	d.removeArea('InterestingArea');
+	console.log(d.getElementAreas(0));
+	// test get current area
+	console.log(d.getCurrentArea());
+	// test item adding/removing
+	d.removeItem('WonderfulItem');
+	d.addItem(i);
+	console.log(d.getItem(0));
+	d.removeItem('WonderfulItem');
+	console.log(d.getItem(0));
+	d.addItem(i);
+	d.addItem(i);
+	console.log(d.getItem(0));
+	console.log(d.getItem(1));
+	d.removeItem('WonderfulItem');
+	console.log(d.getItem(0));
+	console.log(d.getItem(1));
+	console.log('--------Done Dungeon--------');
+}
+*/
--- src/components/scratch/entity.ts
+++ src/components/scratch/entity.ts
@@ -1,39 +1,181 @@
-// base class to inherit from
-class Entity {
+// type cbAnyArray_Any = (...args: any[]) => any;
+type CallBack = () => void;
+
+// base class to inherit and alter behaviors of
+export class Entity {
+	// name/description of Entity
 	protected name: string;
 	protected description: string;
 	
-	//constructor();
-	constructor(n:string, d:string) {
-		this.name = n;
-		this.description = d;
+	// Entity's parent Entity
+	protected parentEntity: Entity
+	
+	// holds all objects that Entity should track
+	protected state: {
+		[key: string]: any;
+	};
+	
+	// holds custom behavior handlers
+	protected bonEnter: CallBack;
+	protected bonExit: CallBack;
+	protected bonPickUp: CallBack;
+	protected bonUse: CallBack;
+	protected bonDrop: CallBack;
+		
+	// ----------------------------------
+	// constructor
+	// ----------------------------------
+	constructor(n: string, d: string, p: Entity) {
+		// set Entity name
+		if (n === undefined || n === null) {
+			console.log('Providing default name, this may cause issues...');
+			this.name = 'DefaultEntityName';
+		} else if (/\s/.test(n)) {
+			console.log(`Removing whitespace from "${n}", this may cause issues...`);
+			this.name = n.replace(/\s/g, '');
+		}else {
+			this.name = n;
+		}
+		
+		// set Entity description
+		if (n === undefined || n === null) {
+			console.log('Providing default Description, this may cause unexpected issues...');
+			this.description = 'Default Entity Description';
+		} else {
+			this.description = d;
+		}
+		
+		// set Entity's parent
+		this.setParent(p);
+		
+		// ----------------------------------
+		// the following should be set after creating entity
+		// ----------------------------------
+		this.state = {};
+		
+		this.bonEnter = undefined;
+		this.bonExit = undefined;
+		this.bonPickUp = undefined;
+		this.bonUse = undefined;
+		this.bonDrop = undefined;
 	}
 	
-	//Examine();
-	Examine(target: string): void {
-		console.log('unimplemented');
+	// ----------------------------------
+	// get the Entity's name description
+	// ----------------------------------
+	getName(): string {	return this.name; }
+	
+	getDescription(): string { return this.description; }
+	
+	// ----------------------------------
+	// Modify the Entity's parent
+	// ----------------------------------
+	getParent(): Entity {
+		return this.parentEntity;
 	}
 	
-	//Interact();
-	Interact(target: string): void {
-		console.log('unimplemented');
+	setParent(p: Entity): void {
+		if (p === undefined || p === null) {
+			console.log('It may be dangerious to not have parent Entity, hope you know what you are doing... setting to undefined');
+			this.parentEntity = undefined;
+		} else {
+			this.parentEntity = p;
+		}
+	}
+		
+	// ----------------------------------
+	// Modify the Entity's state
+	// ----------------------------------
+	addState(key: string, value: any): boolean {
+		if (this.state[key] !== undefined) {
+			console.log(`object already has "${key}"`);
+			return false;
+		}
+		this.state[key] = value;
+		return true;
 	}
 	
-	//Activate();
-	Activate(target: string): void {
-		console.log('unimplemented');		
+	getState(key: string): any {
+		return this.state[key];
 	}
 	
-	//Chat();
-	Chat(target: string): void {
-		console.log('unimplemented');
+	removeState(key: string): void {
+		this.state[key] = undefined;
+	}
+	
+	// ----------------------------------
+	// Modify the Entity's behaviors
+	// ----------------------------------
+	clearBehavior(): void {
+		this.bonEnter = undefined;
+		this.bonExit = undefined;
+		this.bonPickUp = undefined;
+		this.bonUse = undefined;
+		this.bonDrop = undefined;
+	}
+	
+	setOnEnter(f: CallBack): void {
+		this.bonEnter = f;
+	}
+	
+	setOnExit(f: CallBack): void {
+		this.bonExit = f;
+	}
+	
+	setOnPickUp(f: CallBack): void {
+		this.bonPickUp = f;
+	}
+	
+	setOnUse(f: CallBack): void {
+		this.bonUse = f;
+	}
+	
+	setOnDrop(f: CallBack): void {
+		this.bonDrop = f;
+	}
+	
+	// ----------------------------------
+	// Call the Entity's set behaviors
+	// ----------------------------------
+	onEnter(): void {
+		if (this.bonEnter === undefined) {
+			console.log(`"${this.name}" does not have a set onEnter behavior`);
+		} else {
+			this.bonEnter();
+		}
+	}
+	
+	onExit(): void {
+		if (this.bonExit === undefined) {
+			console.log(`"${this.name}" does not have a set onExit behavior`);
+		} else {
+			this.bonExit();
+		}
+	}
+	
+	onPickUp(): void {
+		if (this.bonPickUp === undefined) {
+			console.log(`"${this.name}" does not have a set bonPickUp behavior`);
+		} else {
+			this.bonPickUp();
+		}
+	}
+	
+	onUse(): void {
+		if (this.onUse === undefined) {
+			console.log(`"${this.name}" does not have a set onUse behavior`);
+		} else {
+			this.onUse();
+		}
+	}
+	
+	onDrop(): void {
+		if (this.bonDrop === undefined) {
+			console.log(`"${this.name}" does not have a set onDrop behavior`);
+		} else {
+			this.bonDrop();
+		}
 	}
 }
 
 export default Entity;
-
-
-/*
-IDEA
-pass the calling object so we can edit it if needed!!!
-*/
